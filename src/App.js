92ad106d8695104d0aad8c96937d3d911daadb13
Fayyaz Alam <fayazalam.98@gmail.com>
2021-09-10 13:07:43 -0700

    components fore every field/data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+function Name() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div> <img src="" /> </div>
+      <div><h1>Fayyaz Alam</h1></div>
+    </div>
+  );
+}
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>I am Front-End Web Developer seeking a position in the field of Web Development 
+        , where I can apply my knowledge and skills for continuous improvement.</p>
+    </div>
+  );
+}
+function Skills() {
+  return (
+    <div>
+      <h2>Technologies / Languages </h2>
+      <ul>
+        <li>JAVASCRIPT 
+</li>
+        <li>HTML AND CSS
+</li>
+        <li>BOOTSTRAP</li>
+        <li>FIREBASE</li>
+        <li>JQUERY</li>
+      </ul>
+    </div>
+  );
+}
+function Education() {
+  return (
+    <div>
+      <h2>Education</h2>
+      <strong>
+      WEB AND MOBILE APPLICATION DEVELOPMENT
+      </strong>
+     <p>
+     Saylani | March 2021 to October 2021 Ongoing</p>
+
+     <strong>
+     FRONTEND WEB DEVELOPMENT ACCP
+     </strong>
+     <p>Aptech | 2020 to 2021</p>
+     <strong>
+     AS LEVELS
+     </strong>
+     <p>
+Happy Palace School | 2018 to 2019
+     </p>
+     <strong>
+     O LEVELS
+     </strong>
+     <p>
+Happy Palace School | 2018 to 2019
+     </p>
+    </div>
+     
+  );
+}
+function PersonalSkills() {
+  return (
+    <div>
+      <h2>Personal Skills</h2>
+Creative spirit<br/>
+Organized<br/>
+Time management<br/>
+Team player<br/>
+Fast learner<br/>
+Motivated
+    </div>
+  );
+}
+function Contacts() {
+  return (
+    <div>
+      <h2>
+        Contacts
+      </h2>
+P: +92316-0288603<br/>
+E: fayazalam.98@gmail.com<br/>
+Links :<li> fb.me/fayyaz.alam.940098</li>
+<li>
+github/fayyazcode
+</li>
     </div>
   );
 }
 
-export default App;
+export { About, Name,Skills,Education,PersonalSkills,Contacts };
